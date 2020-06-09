30ae7c02bdf9fc099203db1acbbca2e55ba2559a
Krasimir Etov <4341025+KrasimirEtov@users.noreply.github.com>
2020-06-09 12:52:21 +0300

    [.NET] Fix mobile UI

--- src/.NET Core/SensorManagementSystem/SensorManagementSystem.App/wwwroot/lib/cool-admin/js/my-custom-scripts.js
+++ src/.NET Core/SensorManagementSystem/SensorManagementSystem.App/wwwroot/lib/cool-admin/js/my-custom-scripts.js
@@ -1,5 +1,13 @@
-﻿$(document).ready(function () {
-	$('#logout-link').click(function () {
-		$('#logout-form').submit();
+﻿// Submit logout form desktop
+$(document).ready(function () {
+	$('#logout-link-desktop').click(function () {
+		$('#logout-form-desktop').submit();
+	});
+});
+
+// Submit logout form mobile
+$(document).ready(function () {
+	$('#logout-link-mobile').click(function () {
+		$('#logout-form-mobile').submit();
 	});
 });
